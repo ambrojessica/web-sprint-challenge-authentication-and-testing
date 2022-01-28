1f8d1ad6a7d9d7cb8d3867016254f199cc5e6c30
ambrojessica <ambrojessica0@gmail.com>
2022-01-28 16:00:45 -0600

    created middleware and model, starting routing

---
+++ api/middleware/auth-middleware.js
@@ -0,0 +1,56 @@
+const Users = require('../users/users-model');
+
+//invalid credentials
+const checkUsernameExists = async (req, res, next) => {
+  try {
+    const [user] = await Users.findBy({ username: req.body.username });
+
+    if (!user) {
+      next({
+        status: 401,
+        message: 'invalid credentials'
+      });
+    } else {
+      req.user = user;
+      next();
+    }
+  }
+  catch (err) {
+    next(err);
+  }
+};
+
+//username taken
+const checkUsername = async (req, res, next) => {
+  try {
+    const [user] = await Users.findBy({ username: req.body.username });
+
+    if (!user) {
+      next({
+        status: 422,
+        message: 'username taken',
+      });
+    } else {
+      next();
+    }
+  }
+  catch (err) {
+    next(err);
+  }
+};
+
+//username and password required
+const checkUser = (req, res, next) => {
+  if (!req.body.username || !req.body.password) {
+    next({
+      status: 422,
+      message: 'username and password required',
+    });
+  } else {
+    next();
+  }
+};
+
+module.exports = {
+  checkUsernameExists, checkUsername, checkUser
+};
